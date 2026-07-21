a2550c8ac4edc03c9c3e8f04b0f6bf1e2a9481b9
ahmed8829 <axmadjinaw@gmail.com>
2026-07-21 12:57:38 +0300

    Update API URL to Somee production backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5026',
+  baseURL: 'http://InventoryManagementSystem.somee.com',
   headers: {
     'Content-Type': 'application/json',
   },
